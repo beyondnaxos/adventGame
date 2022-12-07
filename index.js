299912333b9a8db614565cd7d61bada14c2f9ea5
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2022-12-07 16:20:33 +0100

    chal 1 ok

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
-import datas from './input.js';
+import datas from './input.js'
+
+const splitedDatas = datas.split(/\n\n/)
+// console.log(split)
+
+const formatedArrays = splitedDatas.map((numbers) => {
+  const group = numbers.split(/\n/)
+  const groupNumber = group.map(Number)
+  return groupNumber
+})
+
+console.log(formatedArrays)
+
+const newArrayReduced = formatedArrays.map(formatedArray => {
+    const sum = formatedArray.reduce((a, b) => a + b, 0)
+    return sum
+})
+
+// // sort methode
+// const maxNum = newArrayReduced.sort((a, b) => b - a)[0]
+
+// Math.max methode
+console.log(Math.max(...newArrayReduced))
+
+
+
+
+
+
 
